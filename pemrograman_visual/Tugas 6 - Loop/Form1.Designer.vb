4dde4210bb6ce6c9dfec9175eb84fb74ec524e3d
Petrik Star <fajarnurprasetyo.2@gmail.com>
2023-12-13 09:37:33 +0700

    Pemrograman Visual - Tugas 6 | close #2

--- pemrograman_visual/Tugas 6 - Loop/Form1.Designer.vb
+++ pemrograman_visual/Tugas 6 - Loop/Form1.Designer.vb
@@ -22,10 +22,81 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(250, 693)
+        Me.ListBox1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(268, 58)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(96, 20)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "5"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(268, 84)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(268, 12)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(96, 17)
+        Me.RadioButton1.TabIndex = 1
+        Me.RadioButton1.Text = "Do While Loop"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(268, 35)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(90, 17)
+        Me.RadioButton2.TabIndex = 2
+        Me.RadioButton2.Text = "Do Loop Until"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(376, 714)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Fajar Nur Prasetyo | 1122100007"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
 End Class
--- pemrograman_visual/Tugas 6 - Loop/Form1.vb
+++ pemrograman_visual/Tugas 6 - Loop/Form1.vb
@@ -1,3 +1,58 @@
 ﻿Public Class Form1
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If Asc(e.KeyChar) = 13 Then
+            Button1.PerformClick()
+        End If
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
 
+    Private Sub DoWhileLoop(n As Integer)
+        With ListBox1.Items
+            .Clear()
+            Dim Bil1, Bil2 As Integer
+            Bil1 = 1
+            Do While Bil1 <= n
+                .Add(Bil1 & ".")
+                Bil2 = 1
+                Do While Bil2 <= n
+                    .Add(Bil1 & "." & Bil2)
+                    Bil2 += 1
+                Loop
+                Bil1 += 1
+            Loop
+        End With
+    End Sub
+
+    Private Sub DoLoopUntil(n As Integer)
+        With ListBox1.Items
+            .Clear()
+            Dim Bil1, Bil2 As Integer
+            Bil1 = 1
+            Do
+                .Add(Bil1 & ".")
+                Bil2 = 1
+                Do
+                    .Add(Bil1 & "." & Bil2)
+                    Bil2 += 1
+                Loop Until Bil2 = n
+                Bil1 += 1
+            Loop Until Bil1 = n
+        End With
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If RadioButton1.Checked Then
+                DoWhileLoop(TextBox1.Text)
+            ElseIf RadioButton2.Checked Then
+                DoLoopUntil(TextBox1.Text)
+            End If
+        Catch er As InvalidCastException
+            Debug.Print(er.Message)
+        End Try
+    End Sub
 End Class
